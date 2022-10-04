97e6aa663799e4454b73bf5b352387d49349abd5
Appa Komondor <kerberoskomondor@proton.me>
2022-10-03 20:28:11 -0600

    add wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require 'wezterm'
+return {
+  font = wezterm.font('FiraCode Nerd Font'),
+  font_size = 10,
+  enable_tab_bar = false,
+  color_scheme = 'Dracula (Official)',
+  tab_bar_at_bottom = false,
+  use_fancy_tab_bar = false,
+  window_decorations = 'RESIZE',
+  keys = {
+    { key = "C", mods="CTRL", action=wezterm.action{CopyTo="ClipboardAndPrimarySelection"}}
+  }
+}
