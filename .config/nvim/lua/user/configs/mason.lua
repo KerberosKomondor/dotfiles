c121cdbfd488c40d5c4c147468286c556945a1d9
Appa Komondor <kerberoskomondor@proton.me>
2022-11-28 08:35:19 -0700

    fix noice config

--- .config/nvim/lua/user/configs/mason.lua
+++ .config/nvim/lua/user/configs/mason.lua
@@ -24,21 +24,17 @@ local on_attach = function(client, bufnr) local navic_ok, navic = pcall(require,
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help,
-    bufopts)
-  vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set('n', '<leader>wr',
-    vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set('n', '<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set('n', '<leader>D',
-    vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n',
-    '<leader>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<leader>o', vim.lsp.buf.formatting, bufopts) --select language server
+  --  vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  --  vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  --  vim.keymap.set('n', '<leader>wl', function()
+  --    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  --  end, bufopts)
+  --  vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
+  --  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+  --  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+  --  vim.keymap.set('n', '<leader>o', vim.lsp.buf.formatting, bufopts)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
