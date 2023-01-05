f12570b79562304629e51614522c57a210778d6e
Appa Komondor <kerberoskomondor@proton.me>
2023-01-05 07:21:56 -0700

    remove lsp snippets and add cmp-npm

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -13,19 +13,39 @@ local _M = {
     { 'saadparwaiz1/cmp_luasnip' },
     { 'hrsh7th/cmp-nvim-lsp' },
     { 'hrsh7th/cmp-nvim-lua' },
+    { 'David-Kunz/cmp-npm' },
 
-    -- Snippets
+    -- Snippets - disabled for now
     { 'L3MON4D3/LuaSnip' },
-    { 'rafamadriz/friendly-snippets' },
+    --{ 'rafamadriz/friendly-snippets' },
   }
-
 }
 
 function _M.config()
-  local lsp = require 'lsp-zero'
+  local lsp = require('lsp-zero')
+
   lsp.preset('recommended')
 
-  lsp.nvim_workspace()
+  local hasNpm, npm = pcall(require, 'cmp-npm')
+  if hasNpm then
+    npm.setup({
+      ignore = {
+        'beta', 'rc',
+      },
+    })
+  end
+
+  lsp.setup_nvim_cmp({
+    sources = require('cmp').config.sources({
+      { name = 'npm', keyword_length = 4 },
+      { name = 'nvim_lsp', keyword_length = 3 },
+    }, {
+      { name = 'path' },
+      { name = 'buffer', keyword_length = 3 },
+    }),
+  })
+
+  --lsp.nvim_workspace()
 
   lsp.setup()
 end
