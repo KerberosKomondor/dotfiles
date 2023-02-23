5a5e2332c631684fde29103d80ed67eb7e5118b3
Appa Komondor <kerberoskomondor@proton.me>
2023-02-23 15:44:11 -0700

    remove lsp-format and use null ls to format on save

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,130 +1,140 @@
 local M = {
-    name = 'lsp',
-    'VonHeikemen/lsp-zero.nvim',
-    dependencies = {
-        -- LSP Support
-        { 'neovim/nvim-lspconfig' },
-        { 'williamboman/mason.nvim' },
-        { 'williamboman/mason-lspconfig.nvim' },
-        { 'folke/neodev.nvim' },
-        { 'jose-elias-alvarez/null-ls.nvim' },
+	name = "lsp",
+	"VonHeikemen/lsp-zero.nvim",
+	dependencies = {
+		-- LSP Support
+		{ "neovim/nvim-lspconfig" },
+		{ "williamboman/mason.nvim" },
+		{ "williamboman/mason-lspconfig.nvim" },
+		{ "folke/neodev.nvim" },
+		{ "jose-elias-alvarez/null-ls.nvim" },
 
-        -- Autocompletion
-        { 'hrsh7th/nvim-cmp' },
-        { 'hrsh7th/cmp-buffer' },
-        { 'hrsh7th/cmp-path' },
-        { 'hrsh7th/cmp-nvim-lsp' },
-        { 'hrsh7th/cmp-nvim-lua' },
-        { 'David-Kunz/cmp-npm' },
-        {
-            'KerberosKomondor/cmp-jira.nvim',
-            --dir = '/home/appa/code/cmp-jira.nvim/',
-        },
+		-- Autocompletion
+		{ "hrsh7th/nvim-cmp" },
+		{ "hrsh7th/cmp-buffer" },
+		{ "hrsh7th/cmp-path" },
+		{ "hrsh7th/cmp-nvim-lsp" },
+		{ "hrsh7th/cmp-nvim-lua" },
+		{ "David-Kunz/cmp-npm" },
+		{
+			"KerberosKomondor/cmp-jira.nvim",
+			--dir = '/home/appa/code/cmp-jira.nvim/',
+		},
 
-        -- Formatters
-        { 'lukas-reineke/lsp-format.nvim' },
-        -- Snippets - disabled for now
-        { 'rafamadriz/friendly-snippets' },
+		-- Snippets - disabled for now
+		{ "rafamadriz/friendly-snippets" },
 
-        -- Display
-        { 'onsails/lspkind.nvim' },
-        { 'styled-components/vim-styled-components' },
-    }
+		-- Display
+		{ "onsails/lspkind.nvim" },
+		{ "styled-components/vim-styled-components" },
+	},
 }
 
 function M.config()
-  -- Needs to be before lsp is setup
-  require('neodev').setup()
-  require('lsp-format').setup()
-  ----------------------------------
+	-- Needs to be before lsp is setup
+	require("neodev").setup()
+	----------------------------------
 
-  local lsp = require('lsp-zero')
-  local lspconfig = require('lspconfig')
+	local lsp = require("lsp-zero")
+	local lspconfig = require("lspconfig")
 
-  lsp.extend_lspconfig({
-      set_lsp_keymaps = false,
-      on_attach = function(client, bufnr)
-        require('lsp-format').on_attach(client)
+	lsp.extend_lspconfig({
+		set_lsp_keymaps = false,
+		on_attach = function(_, bufnr)
+			local opts = { buffer = bufnr }
 
-        local opts = { buffer = bufnr }
+			-- Keybinds for lsp servers
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+			vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+			vim.keymap.set("n", "<Ctrl-k>", vim.lsp.buf.signature_help, opts)
+			vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
+			vim.keymap.set("n", "<F4>", vim.lsp.buf.code_action, opts)
+			vim.keymap.set("n", "gl", vim.diagnostic.open_float, opts)
+			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+		end,
+	})
 
-        -- Keybinds for lsp servers
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-        vim.keymap.set('n', 'go', vim.lsp.buf.type_definition, opts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<Ctrl-k>', vim.lsp.buf.signature_help, opts)
-        vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
-        vim.keymap.set('n', '<F4>', vim.lsp.buf.code_action, opts)
-        vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
-        vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-      end
-  })
+	require("mason").setup()
+	require("mason-lspconfig").setup({
+		ensure_installed = {
+			"tsserver",
+			"eslint",
+			"lua_ls",
+		},
+	})
 
-  require('mason').setup()
-  require('mason-lspconfig').setup({
-      ensure_installed = {
-          'tsserver',
-          'eslint',
-          'lua_ls',
-      }
-  })
+	require("mason-lspconfig").setup_handlers({
+		function(server_name)
+			lspconfig[server_name].setup({})
+		end,
+		["lua_ls"] = function()
+			lspconfig.lua_ls.setup({
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			})
+		end,
+	})
 
-  require('mason-lspconfig').setup_handlers({
-      function(server_name)
-        lspconfig[server_name].setup({})
-      end,
-      ['lua_ls'] = function()
-        lspconfig.lua_ls.setup {
-            settings = {
-                Lua = {
-                    diagnostics = {
-                        globals = { 'vim' },
-                    },
-                },
-            },
-        }
-      end,
-  })
-
-  --[[
+	--[[
   --      COMPLETION
   --]]
-  local cmp_settings = require('user.cmp')
+	local cmp_settings = require("user.cmp")
 
-  -- Blend settings with default lsp-zero settings
-  local cmp_config = require('lsp-zero').defaults.cmp_config(cmp_settings.blended_config)
+	-- Blend settings with default lsp-zero settings
+	local cmp_config = require("lsp-zero").defaults.cmp_config(cmp_settings.blended_config)
 
-  -- Override entire sections of cmp config
-  cmp_config.mapping = cmp_settings.mapping
-  cmp_config.sources = cmp_settings.sources
+	-- Override entire sections of cmp config
+	cmp_config.mapping = cmp_settings.mapping
+	cmp_config.sources = cmp_settings.sources
 
-  require 'cmp'.setup(cmp_config)
+	require("cmp").setup(cmp_config)
 
-  --[[
+	--[[
   --      DIAGNOSTICS
   --]]
-  lsp.set_sign_icons()
-  vim.diagnostic.config(lsp.defaults.diagnostics({}))
+	lsp.set_sign_icons()
+	vim.diagnostic.config(lsp.defaults.diagnostics({}))
 
-
-  --[[
+	--[[
   --      NULL_LS
   --]]
-  local null_ls = require('null-ls')
-  local null_opts = lsp.build_options('null-ls', {})
+	local null_ls = require("null-ls")
+	local null_opts = lsp.build_options("null-ls", {})
 
-  null_ls.setup({
-      on_attach = null_opts.on_attach,
-      sources = {
-          null_ls.builtins.formatting.eslint_d,
-      },
-  })
+	local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-  lsp.nvim_workspace()
+	null_ls.setup({
+		on_attach = function(client, bufnr)
+			if client.supports_method("textDocument/formatting") then
+				vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					group = augroup,
+					buffer = bufnr,
+					callback = function()
+						vim.lsp.buf.format({ bufnr = bufnr })
+					end,
+				})
+			end
+
+			return null_opts.on_attach
+		end,
+		sources = {
+			null_ls.builtins.formatting.eslint_d,
+			null_ls.builtins.formatting.stylua,
+		},
+	})
+
+	lsp.nvim_workspace()
 end
 
 return M
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,13 +1,16 @@
 local wezterm = require 'wezterm'
 return {
-  font = wezterm.font('FiraCode Nerd Font'),
-  font_size = 11,
-  enable_tab_bar = false,
-  color_scheme = 'Dracula (Official)',
-  tab_bar_at_bottom = false,
-  use_fancy_tab_bar = false,
-  window_decorations = 'RESIZE',
-  keys = {
-    { key = "C", mods = "CTRL", action = wezterm.action { CopyTo = "ClipboardAndPrimarySelection" } }
-  }
+    font = wezterm.font_with_fallback {
+        'FiraCode Nerd Font',
+        'JetBrains Mono',
+    },
+    font_size = 11,
+    enable_tab_bar = false,
+    color_scheme = 'Dracula (Official)',
+    tab_bar_at_bottom = false,
+    use_fancy_tab_bar = true,
+    window_decorations = 'RESIZE',
+    keys = {
+        { key = "C", mods = "CTRL", action = wezterm.action { CopyTo = "ClipboardAndPrimarySelection" } }
+    },
 }
