10be5a84fd9563e1c8593effecbf0d16d52dee5e
Appa Komondor <kerberoskomondor@proton.me>
2023-04-18 13:11:26 -0600

    add lazy check to statusline

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,17 @@
 -- keymaps needs set before lazy
-require 'user.globals'
-require 'user.keymaps'
-require 'user.settings'
-require 'user.lazy_bootstrap'
+require("user.globals")
+require("user.keymaps")
+require("user.settings")
+require("user.lazy_bootstrap")
 
 -- load plugins in lsp/plugins
-require 'lazy'.setup 'plugins'
+require("lazy").setup("plugins", {
+	install = {
+		colorscheme = { "dracula" },
+	},
+	checker = {
+		enabled = true,
+		notify = true,
+		frequency = 7200, -- 2 hours
+	},
+})
--- .config/nvim/lua/plugins/statusline.lua
+++ .config/nvim/lua/plugins/statusline.lua
@@ -1,12 +1,21 @@
 local _M = {
-  'nvim-lualine/lualine.nvim',
-  name = 'lines',
+	"nvim-lualine/lualine.nvim",
+	name = "lines",
 }
 
 function _M.config()
-  require'lualine'.setup({
-    theme = 'dracula',
-  })
+	require("lualine").setup({
+		theme = "dracula",
+		sections = {
+			lualine_x = {
+				{
+					require("lazy.status").updates,
+					cond = require("lazy.status").has_updates,
+					color = { fg = "#BD93F9" },
+				},
+			},
+		},
+	})
 end
 
 return _M
