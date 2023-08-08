51a201894fbb7cb6a2615c0e38cc021f84fccce4
Appa Komondor <kerberoskomondor@proton.me>
2023-08-08 08:51:45 -0600

    stop notifying for updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -7,13 +7,13 @@ require("user.lsp-keybindings")
 
 -- load plugins in lsp/plugins
 require("lazy").setup("plugins", {
-  install = {
-    missing = false,
-    colorscheme = { "dracula" },
-  },
-  checker = {
-    enabled = true,
-    notify = true,
-    frequency = 7200, -- 2 hours
-  },
+	install = {
+		missing = false,
+		colorscheme = { "dracula" },
+	},
+	checker = {
+		enabled = true,
+		notify = false,
+		frequency = 14400, -- 4 hours
+	},
 })
