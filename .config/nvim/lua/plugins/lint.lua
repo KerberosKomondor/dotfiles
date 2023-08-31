178f7d80936b7a2a7b5204d58d81340deb7ca580
Appa Komondor <kerberoskomondor@proton.me>
2023-08-31 12:48:42 -0600

    add more lint rules

--- .config/nvim/lua/plugins/lint.lua
+++ .config/nvim/lua/plugins/lint.lua
@@ -6,9 +6,25 @@ function M.config()
   require("lint").linters_by_ft = {
     markdown = { 'vale' },
     typescriptreact = { 'eslint_d'},
+<<<<<<< Updated upstream
     typescript = { 'eslint_d' },
     lua = { 'luacheck' },
+=======
+    typescript = { 'eslint' },
+    lua = { 'luacheck'},
+    json = { 'jsonlint' },
+    yaml = { 'yamllint' },
+>>>>>>> Stashed changes
   }
+
+  vim.diagnostic.config({
+    virtual_text = {
+      source = true,
+    },
+    float = {
+      source = true,
+    }
+  })
 
   vim.api.nvim_create_autocmd({ "BufWritePost" }, {
     callback = function()
