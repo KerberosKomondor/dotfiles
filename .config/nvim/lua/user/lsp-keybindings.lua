e80a60e3a15d627f8db0aa73d3dbca0a0546e3d7
Appa Komondor <kerberoskomondor@proton.me>
2023-09-01 11:59:51 -0600

    add more default like lsp keybindings

--- .config/nvim/lua/user/lsp-keybindings.lua
+++ .config/nvim/lua/user/lsp-keybindings.lua
@@ -6,30 +6,35 @@ vim.api.nvim_create_autocmd("LspAttach", {
     local opts = { buffer = event.bufnr }
 
     -- Keybinds for lsp servers
-    -- LSP finder - Find the symbol's definition
-    -- If there is no definition, it will instead be hidden
-    -- When you use an action in finder like "open vsplit",
-    -- you can use <C-t> to jump back
-    keymap("n", "gh", "<cmd>Lspsaga lsp_finder<CR>")
 
-    -- Code action
-    keymap({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
-
-    -- Rename all occurrences of the hovered word for the entire file
-    keymap("n", "<space>rn", "<cmd>Lspsaga rename ++project<CR>", opts)
-
+    -- navigation
     keymap("n", "gr", vim.lsp.buf.references, opts)
     keymap("n", "gD", vim.lsp.buf.declaration, opts)
     keymap("n", "gd", vim.lsp.buf.definition, opts)
     keymap("n", "gi", vim.lsp.buf.implementation, opts)
+    keymap("n", "gr", vim.lsp.buf.references, opts)
+    keymap("n", "<leader>D", vim.lsp.buf.type_definition, opts)
     keymap("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    keymap("n", "<shift>K", vim.lsp.buf.hover, opts)
+    keymap("n", "<F2>", vim.lsp.buf.rename, opts)
+    keymap("n", "<leader>ci", vim.lsp.buf.incoming_calls, opts)
+    keymap("n", "<leader>co", vim.lsp.buf.outgoing_calls, opts)
+
+    -- utilities
+    keymap("n", "<leader>lf", vim.lsp.buf.format, opts)
+
+    -- workspaces
     keymap("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
     keymap("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
     keymap("n", "<space>wl", function()
       print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
     end, opts)
-    keymap("n", "<space>D", vim.lsp.buf.type_definition, opts)
+
+    -- diagnostics
+    keymap("n", "[d", vim.diagnostic.goto_next, opts)
+    keymap("n", "]d", vim.diagnostic.goto_prev, opts)
+    keymap("n", "<leader>do", vim.diagnostic.goto_prev, opts)
+    keymap("n", "<leader>dd", "<cmd>Telescope diagnostics<cr>", opts)
     keymap("n", "<space>ca", vim.lsp.buf.code_action, opts)
-    keymap("n", "gr", vim.lsp.buf.references, opts)
   end,
 })
