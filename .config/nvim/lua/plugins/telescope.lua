0eae225b1ca31ace2dd839ecd02d0d420f862a4a
Appa Komondor <kerberoskomondor@proton.me>
2024-02-06 08:57:20 -0700

    add normal mode binding as well

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,44 +1,44 @@
 local _M = {
-	"nvim-telescope/telescope.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"BurntSushi/ripgrep",
-		{
-			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "make",
-		},
-		"rcarriga/nvim-notify",
-	},
+  "nvim-telescope/telescope.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "BurntSushi/ripgrep",
+    {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+    },
+    "rcarriga/nvim-notify",
+  },
 }
 
 function _M.config()
-	local ok, telescope = pcall(require, "telescope")
-	if not ok then
-		return
-	end
+  local ok, telescope = pcall(require, "telescope")
+  if not ok then
+    return
+  end
 
-	local actions = require("telescope.actions")
-	local action_layout = require("telescope.actions.layout")
+  local actions = require("telescope.actions")
+  local action_layout = require("telescope.actions.layout")
 
-	local hasNotify = pcall(require, "notify")
+  local hasNotify = pcall(require, "notify")
 
-	telescope.setup({
-		defaults = {
-			mappings = {
-				i = {
-					["<esc>"] = actions.close, -- single esc to close
-					["<C-u>"] = false, -- clear input
-					["<M-p>"] = action_layout.toggle_preview, -- toggle preview screen
-				},
-			},
-			path_display = {
-				"smart",
-			},
-		},
-		pickers = {
-			find_files = {
-				hidden = true,
-			},
+  telescope.setup({
+    defaults = {
+      mappings = {
+        i = {
+          ["<esc>"] = actions.close,           -- single esc to close
+          ["<C-u>"] = false,                   -- clear input
+          ["<M-p>"] = action_layout.toggle_preview, -- toggle preview screen
+        },
+      },
+      path_display = {
+        "smart",
+      },
+    },
+    pickers = {
+      find_files = {
+        hidden = true,
+      },
       buffers = {
         show_all_buffers = true,
         sort_lastused = true,
@@ -47,23 +47,27 @@ function _M.config()
         mappings = {
           i = {
             ["<C-d>"] = actions.delete_buffer + actions.move_to_top,
+          },
+          n = {
+            ["d"] = actions.delete_buffer + actions.move_to_top,
           }
+
         }
       }
-		},
-		extensions = {
-			fzf = {
-				fuzzy = true,
-				case_mode = "smart_case",
-			},
-		},
-	})
+    },
+    extensions = {
+      fzf = {
+        fuzzy = true,
+        case_mode = "smart_case",
+      },
+    },
+  })
 
-	telescope.load_extension("fzf")
+  telescope.load_extension("fzf")
 
-	if hasNotify then
-		telescope.load_extension("notify")
-	end
+  if hasNotify then
+    telescope.load_extension("notify")
+  end
 end
 
 return _M
