37f98ec51aac3e665eed6a9f5d7d7365b80209f3
Appa Komondor <kerberoskomondor@proton.me>
2025-05-05 20:27:24 -0600

    set keybinds again

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -3,19 +3,8 @@ return {
     "saghen/blink.cmp",
     enabled = true,
     opts = {
-      completion = {
-        menu = {
-          border = "rounded",
-        },
-        documentation = {
-          window = {
-            border = "rounded",
-          },
-        },
-      },
       keymap = {
-        preset = "none",
-        ["<CR>"] = {},
+        preset = "default",
       },
     },
   },
