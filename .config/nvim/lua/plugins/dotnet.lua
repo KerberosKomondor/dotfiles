af6a83ad051de74af0d31cffccf9cbf6d6e5cf7e
Appa Komondor <kerberoskomondor@proton.me>
2025-05-13 14:14:33 -0600

    switch to roslyn for dotnet

---
+++ .config/nvim/lua/plugins/dotnet.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      registries = {
+        "github:mason-org/mason-registry",
+        "github:Crashdummyy/mason-registry",
+      },
+    },
+  },
+  {
+    "seblyng/roslyn.nvim",
+    ft = "cs",
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
+    opts = {},
+  },
+}
