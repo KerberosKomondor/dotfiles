2ec0a2b5cc2375d19f34af67e6d966aedaa8db46
Appa Komondor <kerberoskomondor@proton.me>
2025-05-17 21:03:47 -0600

    add nuget manager.  it's aight

--- .config/nvim/lua/plugins/dotnet.lua
+++ .config/nvim/lua/plugins/dotnet.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: missing-fields
 return {
   {
     "mason-org/mason.nvim",
@@ -13,6 +14,41 @@ return {
     ft = "cs",
     ---@module 'roslyn.config'
     ---@type RoslynNvimConfig
-    opts = {},
+    opts = {
+      config = {
+        settings = {
+          ["csharp|inlay_hints"] = {
+            csharp_enable_inlay_hints_for_implicit_object_creation = true,
+            csharp_enable_inlay_hints_for_implicit_variable_types = true,
+            csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+            csharp_enable_inlay_hints_for_types = true,
+            dotnet_enable_inlay_hints_for_indexer_parameters = true,
+            dotnet_enable_inlay_hints_for_literal_parameters = true,
+            dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+            dotnet_enable_inlay_hints_for_other_parameters = true,
+            dotnet_enable_inlay_hints_for_parameters = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+          },
+          ["csharp|code_lens"] = {
+            dotnet_enable_references_code_lens = true,
+          },
+        },
+      },
+    },
+  },
+  {
+    "MonsieurTib/neonuget",
+    config = function()
+      require("neonuget").setup({
+        -- Optional configuration
+        dotnet_path = "dotnet", -- Path to dotnet CLI
+        default_project = nil, -- Auto-detected, or specify path like "./MyProject/MyProject.csproj"
+      })
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
   },
 }
