89f21a3f93a5861b6d9390632cbfb63561145143
Appa Komondor <kerberoskomondor@proton.me>
2026-06-08 20:41:00 -0600

    feat(ags): wire CalendarPopup into app

--- .config/ags/app.ts
+++ .config/ags/app.ts
@@ -5,6 +5,7 @@ import Bar from "./widget/Bar"
 import WeatherPopup from "./widget/WeatherPopup"
 import Dashboard from "./widget/Dashboard"
 import TodoPopup from "./widget/TodoPopup"
+import CalendarPopup from "./widget/CalendarPopup"
 import { createState } from "ags"
 
 export const [dashboardVisible, setDashboardVisible] = createState(false)
@@ -20,5 +21,6 @@ app.start({
     Dashboard(monitors[0])
     WeatherPopup(monitors[0])
     TodoPopup(monitors[0])
+    CalendarPopup(monitors[0])
   },
 })
